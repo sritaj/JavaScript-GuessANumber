700b2f5d59eb74ac0961cd9166a8ee6245088853
Sritaj <sritajp@gmail.com>
2021-06-14 21:49:07 +0530

    Added validation when Score > 0 and less than 0

--- script.js
+++ script.js
@@ -4,26 +4,34 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
-let score = 0;
+let score = 10;
+document.querySelector(".score").textContent = score;
+
 // logic to validate user Input
 document.querySelector(".check").addEventListener("click", function () {
   const enteredNumber = Number(document.querySelector(".guess").value);
   console.log(enteredNumber, typeof enteredNumber);
 
-  if (!enteredNumber) {
-    document.querySelector(".message").textContent = "Please Enter a Number!!";
-  } else if (enteredNumber > secretNumber) {
-    document.querySelector(".message").textContent = "Too High Input 📈";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (enteredNumber < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low Input 📉";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (enteredNumber === secretNumber) {
-    document.querySelector(".number").textContent = secretNumber;
-    document.querySelector(".message").textContent = "Congratulations 🔥";
-    score++;
-    document.querySelector(".score").textContent = score;
+  if (score > 0) {
+    if (!enteredNumber) {
+      document.querySelector(".message").textContent =
+        "Please Enter a Number!!";
+    } else if (enteredNumber > secretNumber) {
+      document.querySelector(".message").textContent = "Too High Input 📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else if (enteredNumber < secretNumber) {
+      document.querySelector(".message").textContent = "Too Low Input 📉";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else if (enteredNumber === secretNumber) {
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector(".message").textContent = "Congratulations 🔥";
+      score++;
+      document.querySelector(".score").textContent = score;
+    }
+  } else {
+    document.querySelector(".message").textContent = "You lost the Game 🔥";
+    document.querySelector(".score").textContent = 0;
   }
 });
